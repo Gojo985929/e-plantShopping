f13e5ff63c1ec220c747e60167430c534dc55444
Gojo985929 <go.dead.jo@gmail.com>
2025-07-06 07:21:39 -0400

    Task 1

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        // Check if the item already exists in the cart
+        const existing = state.items.find(
+        (item) => item.name === action.payload.name);
+        if (existing) {
+            // If it exists, increase its quantity
+            existing.quantity += 1;
+        } else {
+            // Otherwise, add it with quantity = 1
+            state.items.push({ ...action.payload, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+      // Remove item by name
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
     updateQuantity: (state, action) => {
-
-    
+        // Update quantity for a specific item
+      const { name, quantity } = action.payload;
+      const existing = state.items.find((item) => item.name === name);
+      if (existing) {
+        existing.quantity = quantity;
+      }
     },
   },
 });
